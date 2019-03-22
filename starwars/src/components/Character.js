b4b276f5b10c113cf749190a269132a17522cf24
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-03-22 12:01:04 -0400

    Ethan Baugher

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Character = props => {
+    return(
+        <div>Character Info</div>
+    )
+}
+
+export default Character;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Character from './Character';
+
+const CharacterList = props => {
+    return (
+        <Character />
+    )
+}
+
+export default CharacterList;
